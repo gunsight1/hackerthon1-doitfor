c8b31936873f0933b09215ca63aaee8f0b8d1141
gunsight1 <gunsight777@gmail.com>
2023-10-16 14:26:11 +0900

    default settings

---
+++ src/main/java/com/kernel360/doitforme/DoitformeApplication.java
@@ -0,0 +1,13 @@
+package com.kernel360.doitforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoitformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoitformeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kernel360/doitforme/DoitformeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kernel360.doitforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoitformeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
